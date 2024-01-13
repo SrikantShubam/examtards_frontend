0e1a65df3a5b3cddd7fb491b25931397691fd73f
SrikantShubam <srikantshubham.personal@gmail.com>
2024-01-13 16:58:07 +0530

    added in speedinsights

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
